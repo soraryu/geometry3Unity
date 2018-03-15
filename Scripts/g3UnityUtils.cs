c89ad5b0fa1230e5d294c3b1fa0c5fb72a7b662f
herbst <herbst@prefrontalcortex.de>
2018-03-15 15:40:56 +0100

    adding asmdefs, moving stuff to right places

--- Scripts/g3UnityUtils.cs
+++ Scripts/g3UnityUtils.cs
@@ -6,10 +6,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using g3;
 
-public class g3UnityUtils
-{
-
-
+public static class g3UnityUtils
+{    
     public static GameObject CreateMeshGO(string name, DMesh3 mesh, Material setMaterial = null, bool bCollider = true)
     {
         var gameObj = new GameObject(name);
@@ -110,7 +108,7 @@ public class g3UnityUtils
             return triangles;
         }
 
-        public void ApplyTo(Mesh mesh, bool recalculateNormals = false, float normalsAngle = 30f)
+        public void ApplyTo(Mesh mesh, bool recalculateNormals = false)
         {
             mesh.Clear();
 
@@ -124,7 +122,7 @@ public class g3UnityUtils
             mesh.SetColors(colors);
 
             if (recalculateNormals)
-                mesh.RecalculateNormals(normalsAngle, true);
+                mesh.RecalculateNormals();
         }
     }
 
@@ -499,4 +497,193 @@ public class g3UnityUtils
         Debug.Log(ob);
 #endif
     }
+
+    #region Recalculate Normals
+
+    /// <summary>
+    ///     Recalculate the normals of a mesh based on an angle threshold. This takes
+    ///     into account distinct vertices that have the same position.
+    /// </summary>
+    /// <param name="mesh"></param>
+    /// <param name="angle">
+    ///     The smoothing angle. Note that triangles that already share
+    ///     the same vertex will be smooth regardless of the angle! (unless you specify "makeVerticesUnique" as well)
+    /// </param>
+    public static void RecalculateNormals(this g3UnityUtils.TemporaryMesh mesh, float angle, bool makeVerticesUnique = false)
+    {
+        if (makeVerticesUnique)
+        {
+            // reconfigure mesh so all triangles have unique vertices
+            var tris = mesh.triangles;
+            var verts = mesh.vertices;
+            var uvs = mesh.uv;
+
+            var c = tris.Count;
+            var newVerts = new Vector3[c];
+            var newUVs = new Vector2[c];
+            var newTris = new int[c];
+            for (int i = 0; i < c; i++)
+            {
+                var vi = tris[i];
+                newVerts[i] = verts[vi];
+                newUVs[i] = uvs[vi];
+                newTris[i] = i;
+            }
+
+            mesh.vertices = new List<Vector3>(newVerts);
+            mesh.uv = new List<Vector2>(newUVs);
+            mesh.triangles = new List<int>(newTris);
+        }
+
+        // recalc normals
+        var cosineThreshold = Mathf.Cos(angle * Mathf.Deg2Rad);
+
+        var vertices = mesh.vertices;
+        var normals = new Vector3[vertices.Count];
+
+        // Holds the normal of each triangle in each sub mesh.
+        var triNormals = new Vector3[mesh.subMeshCount][];
+
+        var dictionary = new Dictionary<VertexKey, List<VertexEntry>>(vertices.Count);
+
+        for (var subMeshIndex = 0; subMeshIndex < mesh.subMeshCount; ++subMeshIndex)
+        {
+
+            var triangles = mesh.GetTriangles(subMeshIndex);
+
+            triNormals[subMeshIndex] = new Vector3[triangles.Count / 3];
+
+            for (var i = 0; i < triangles.Count; i += 3)
+            {
+                int i1 = triangles[i];
+                int i2 = triangles[i + 1];
+                int i3 = triangles[i + 2];
+
+                // Calculate the normal of the triangle
+                Vector3 p1 = vertices[i2] - vertices[i1];
+                Vector3 p2 = vertices[i3] - vertices[i1];
+                Vector3 normal = Vector3.Cross(p1, p2).normalized;
+                int triIndex = i / 3;
+                triNormals[subMeshIndex][triIndex] = normal;
+
+                List<VertexEntry> entry;
+                VertexKey key;
+
+                if (!dictionary.TryGetValue(key = new VertexKey(vertices[i1]), out entry))
+                {
+                    entry = new List<VertexEntry>(4);
+                    dictionary.Add(key, entry);
+                }
+                entry.Add(new VertexEntry(subMeshIndex, triIndex, i1));
+
+                if (!dictionary.TryGetValue(key = new VertexKey(vertices[i2]), out entry))
+                {
+                    entry = new List<VertexEntry>();
+                    dictionary.Add(key, entry);
+                }
+                entry.Add(new VertexEntry(subMeshIndex, triIndex, i2));
+
+                if (!dictionary.TryGetValue(key = new VertexKey(vertices[i3]), out entry))
+                {
+                    entry = new List<VertexEntry>();
+                    dictionary.Add(key, entry);
+                }
+                entry.Add(new VertexEntry(subMeshIndex, triIndex, i3));
+            }
+        }
+
+        // Each entry in the dictionary represents a unique vertex position.
+
+        foreach (var vertList in dictionary.Values)
+        {
+            for (var i = 0; i < vertList.Count; ++i)
+            {
+
+                var sum = new Vector3();
+                var lhsEntry = vertList[i];
+
+                for (var j = 0; j < vertList.Count; ++j)
+                {
+                    var rhsEntry = vertList[j];
+
+                    if (lhsEntry.VertexIndex == rhsEntry.VertexIndex)
+                    {
+                        sum += triNormals[rhsEntry.MeshIndex][rhsEntry.TriangleIndex];
+                    }
+                    else
+                    {
+                        // The dot product is the cosine of the angle between the two triangles.
+                        // A larger cosine means a smaller angle.
+                        var dot = Vector3.Dot(
+                            triNormals[lhsEntry.MeshIndex][lhsEntry.TriangleIndex],
+                            triNormals[rhsEntry.MeshIndex][rhsEntry.TriangleIndex]);
+                        if (dot >= cosineThreshold)
+                        {
+                            sum += triNormals[rhsEntry.MeshIndex][rhsEntry.TriangleIndex];
+                        }
+                    }
+                }
+
+                normals[lhsEntry.VertexIndex] = sum.normalized;
+            }
+        }
+
+        mesh.normals = new List<Vector3>(normals);
+    }
+
+    private struct VertexKey
+    {
+        private readonly long _x;
+        private readonly long _y;
+        private readonly long _z;
+
+        // Change this if you require a different precision.
+        private const int Tolerance = 100000;
+
+        // Magic FNV values. Do not change these.
+        private const long FNV32Init = 0x811c9dc5;
+        private const long FNV32Prime = 0x01000193;
+
+        public VertexKey(Vector3 position)
+        {
+            _x = (long)(Mathf.Round(position.x * Tolerance));
+            _y = (long)(Mathf.Round(position.y * Tolerance));
+            _z = (long)(Mathf.Round(position.z * Tolerance));
+        }
+
+        public override bool Equals(object obj)
+        {
+            var key = (VertexKey)obj;
+            return _x == key._x && _y == key._y && _z == key._z;
+        }
+
+        public override int GetHashCode()
+        {
+            long rv = FNV32Init;
+            rv ^= _x;
+            rv *= FNV32Prime;
+            rv ^= _y;
+            rv *= FNV32Prime;
+            rv ^= _z;
+            rv *= FNV32Prime;
+
+            return rv.GetHashCode();
+        }
+    }
+
+    private struct VertexEntry
+    {
+        public int MeshIndex;
+        public int TriangleIndex;
+        public int VertexIndex;
+
+        public VertexEntry(int meshIndex, int triIndex, int vertIndex)
+        {
+            MeshIndex = meshIndex;
+            TriangleIndex = triIndex;
+            VertexIndex = vertIndex;
+        }
+    }
+
+    #endregion
 }
